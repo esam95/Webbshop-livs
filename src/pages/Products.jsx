c24fb4e1a28352129178451ed9139be9db6e05d9
Esam Ali Gelani <esam_95@hotmail.se>
2023-04-12 11:41:43 +0200

    API works

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,6 +1,14 @@
 import React from 'react'
 
 const Products = () => {
+
+  const fetchData = async () => {
+    const response = await fetch('https://product-api-production-94fe.up.railway.app/products/6432d06c4a8b8aec89853d89')
+    const data = await response.json()
+
+    console.log(data)
+  }
+fetchData()
   return (
     <div>Products</div>
   )
