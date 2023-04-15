8b447695184b261be3a6d7d3eb19ff59c494dea9
Esam Ali Gelani <esam_95@hotmail.se>
2023-04-15 22:07:34 +0200

    admin done

--- src/pages/admin/ManageProducts.jsx
+++ src/pages/admin/ManageProducts.jsx
@@ -1,8 +1,69 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
 
 const ManageProducts = () => {
+  const [productList, setProductList] = useState([]);
+
+  const fetchData = async () => {
+    try{
+      const response = await fetch('https://product-api-production-94fe.up.railway.app/products')
+      const data = await response.json()
+      setProductList(data)
+      console.log(data)
+    } catch(error){
+      console.log(error)
+    }
+  }
+
+  const deleteProduct = async (e) => {
+    try{
+      e.preventDefault();
+      await fetch(`https://product-api-production-94fe.up.railway.app/products/${e.target.id}`, {
+        method: 'DELETE'
+      })
+      console.log(e.target.id)
+      fetchData();
+    }catch(error){
+      console.log(error);
+    }
+      
+  }
+
+  useEffect(() => {
+    fetchData()
+  }, []);
   return (
-    <div>ManageProducts</div>
+    
+    <div>
+      <Link to={"/products"}>Products</Link><br />
+      <Link to={"../createproduct"} relative='path'>creat a product</Link>
+      <table>
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>price</th>
+            <th>stock</th>
+          </tr>
+        </thead>
+        
+        <tbody>
+          {productList.map(product => {
+          return <tr key={product._id}>
+            <td>{product.title}</td>
+            <td>{product.price}</td>
+            <td>{product.stock}</td>
+            <button>
+              <Link to={`../updateproduct/${product._id}`} relative='path'>Update Product</Link>
+            </button>
+            <button id={product._id} onClick={deleteProduct}>
+              Delete Product
+            </button>
+            </tr>
+          })}
+        </tbody>
+      </table>
+      
+    </div>
   )
 }
 
--- src/pages/admin/UpdateProduct.jsx
+++ src/pages/admin/UpdateProduct.jsx
@@ -1,8 +1,103 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useParams } from 'react-router-dom'
+import { useState } from 'react';
 
 const UpdateProduct = () => {
+  const id = useParams().productId;
+  const [product, setProduct] = useState({});
+
+  const fetchProduct = async () => {
+    const response = await fetch('https://product-api-production-94fe.up.railway.app/products/'+id);
+    const data = await response.json();
+    setProduct(data)
+  }
+
+  useEffect(() => {
+    fetchProduct()
+  }, []);
+
+  const updateProduct = async (e) => {
+    try{
+      e.preventDefault()
+      const response = await fetch('https://product-api-production-94fe.up.railway.app/products/'+id, {
+      method: 'PATCH',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify({
+        _id: product._id,
+        title: product.title,
+        description: product.description,
+        price: product.price,
+        stock: product.stock,
+        category: product.category,
+        image: product.image,
+        date: product.date,
+        __v: product.__v
+      })
+    })
+    const postSuccessfull = await response.json();
+    if(postSuccessfull.acknowledged===true){
+      window.location.href = "/admin/manageproducts";
+    }
+    }catch(error) {
+      console.log(error);
+    } 
+  }
+
+  function handleChangeTitle (e) {
+    e.preventDefault();
+    console.log(product)
+    setProduct({
+      ...product,
+      title: e.target.value
+    })
+  console.log(product)}
+
+  function handleChangeDescription (e) {
+    e.preventDefault();
+    setProduct({
+      ...product,
+      description: e.target.value
+    })}
+
+  function handleChangePrice (e) {
+    e.preventDefault();
+    setProduct({
+      ...product,
+      price: e.target.value
+    })}
+
+  function handleChangeStock (e) {
+    e.preventDefault();
+    setProduct({
+      ...product,
+      stock: e.target.value
+    })}
+
+    function handleChangeCategory (e) {
+      e.preventDefault();
+      setProduct({
+        ...product,
+        category: e.target.value
+      })}
+
   return (
-    <div>UpdateProduct</div>
+    <div>
+      <form onSubmit={updateProduct}>
+        <label>Title</label><br />
+        <input type="text" onChange={handleChangeTitle} value={product.title}/><br />
+        <label>Description</label><br />
+        <textarea name="" id="" cols="30" rows="10" onChange={handleChangeDescription} value={product.description}></textarea><br />
+        <label>Price</label><br />
+        <input type="text" onChange={handleChangePrice} value={product.price}/><br />
+        <label>Stock</label><br />
+        <input type="text" onChange={handleChangeStock} value={product.stock}/><br />
+        <label>Category</label><br />
+        <input type="text" onChange={handleChangeCategory} value={product.category}/><br />
+        <button>Submit</button>
+      </form>
+    
+    
+    </div>
   )
 }
 
