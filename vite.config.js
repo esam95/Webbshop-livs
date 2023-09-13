ea5f998da9f349a9f9c87d23d7a4fb195defcaec
Esam Gelani <esam_95@hotmail.se>
2023-09-13 14:05:28 +0200

    failed commit

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Webbshop-livs/',
+  base: '',
   build: {
     outDir: 'build', // Ensure this points to the desired output directory
   },
